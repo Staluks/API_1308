bc92850c2885a82f411779f26a4b1d88ab3f6c1f
Staluks <janchukssss@inbox.lv>
2022-08-16 10:41:44 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,50 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+
+
+// https://jsonplaceholder.typicode.com/posts
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://jsonplaceholder.typicode.com/posts")).build();
+
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+//                :: - lamda piekļūst interfacem
+                .thenApply(HttpResponse::body)
+                // .thenAccept(System.out::println)
+                .thenAccept(Main::format)
+                .join();
+
+
+
     }
+    public static void format(String responseBody) {
+        JSONArray posts = new JSONArray(responseBody);
+
+
+        for(int i = 0; i < posts.length(); i++) {
+            JSONObject post = posts.getJSONObject(i);
+
+
+            int id = post.getInt("id");
+            String title = post.getString("title");
+            String body = post.getString("body");
+            System.out.println(id + " " + title + " " + body);
+
+        }
+
+    }
+
+
+
 }
